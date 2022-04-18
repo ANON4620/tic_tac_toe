45e4f1c8a9bd33ad66dfe303c296f25571ba29bb
ANON4620 <swarnadeep.click@gmail.com>
2022-04-18 14:19:32 +0530

    Better DRY approach

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -3,31 +3,43 @@
 #include <time.h>
 
 char board[3][3];
-char cur_player;
+char cur_player = 'X';
 int moves_left = 9;
 
 int main()
 {
     void set_up_board();
-    void draw();
+    void draw(int);
     void player();
     void computer();
     int check_win();
     int check_draw();
-    
+   
+    char temp;
+
     initscr();
     noecho();
     curs_set(0);
     srand(time(NULL));
     set_up_board();
+    draw(1);
     
     while(1)
     {
-        // PLAYER'S MOVE
-        cur_player = 'X';
-        player();
+        if(cur_player == 'X')
+	{
+        	player();
+		temp = 'O';
+	}
+	else
+	{
+		computer();
+		temp = 'X';
+	}
+
 	clear();
-	draw();
+	draw(0);
+
         if(check_win())
         {
             printw("%c wins!\n\n", cur_player);
@@ -38,22 +50,8 @@ int main()
             addstr("Draw!\n\n");
             break;
         }
-        
-        // COMPUTER'S MOVE
-        cur_player = 'O';
-        computer();
-	clear();
-	draw();
-        if(check_win())
-        {
-            printw("%c wins!\n\n", cur_player);
-            break;
-        }
-        if(check_draw())
-        {
-            addstr("Draw!\n\n");
-            break;
-        }
+
+	cur_player = temp;
     }
 
     addstr("[Press any key to quit]");
@@ -69,35 +67,38 @@ void set_up_board()
     for(int i = 0; i < 3; i++)
         for(int j = 0; j < 3; j++)
             board[i][j] = ' ';
-
-    addstr("     |     |     \n");
-    addstr("  1  |  2  |  3  \n");
-    addstr("     |     |     \n");
-    addstr("-----------------\n");
-    addstr("     |     |     \tPlayer - X\n");
-    addstr("  4  |  5  |  6  \tComputer - O\n");
-    addstr("     |     |     \n");
-    addstr("-----------------\n");
-    addstr("     |     |     \n");
-    addstr("  7  |  8  |  9  \n");
-    addstr("     |     |     \n\n");
-
-    refresh();
 }
 
-void draw()
+void draw(int init)
 {
-    addstr("     |     |     \n");
-    printw("  %c  |  %c  |  %c  \n", board[0][0], board[0][1], board[0][2]);
-    addstr("     |     |     \n");
-    addstr("-----------------\n");
-    addstr("     |     |     \tPlayer - X\n");
-    printw("  %c  |  %c  |  %c  \tComputer - O\n", board[1][0], board[1][1], board[1][2]);
-    addstr("     |     |     \n");
-    addstr("-----------------\n");
-    addstr("     |     |     \n");
-    printw("  %c  |  %c  |  %c  \n", board[2][0], board[2][1], board[2][2]);
-    addstr("     |     |     \n\n");
+    if(init)
+    {
+    	addstr("     |     |     \n");
+	addstr("  1  |  2  |  3  \n");
+    	addstr("     |     |     \n");
+	addstr("-----------------\n");
+	addstr("     |     |     \tPlayer - X\n");
+	addstr("  4  |  5  |  6  \tComputer - O\n");
+    	addstr("     |     |     \n");
+    	addstr("-----------------\n");
+    	addstr("     |     |     \n");
+    	addstr("  7  |  8  |  9  \n");
+	addstr("     |     |     \n\n");
+    }
+    else
+    {
+    	addstr("     |     |     \n");
+    	printw("  %c  |  %c  |  %c  \n", board[0][0], board[0][1], board[0][2]);
+    	addstr("     |     |     \n");
+    	addstr("-----------------\n");
+    	addstr("     |     |     \tPlayer - X\n");
+    	printw("  %c  |  %c  |  %c  \tComputer - O\n", board[1][0], board[1][1], board[1][2]);
+    	addstr("     |     |     \n");
+    	addstr("-----------------\n");
+    	addstr("     |     |     \n");
+    	printw("  %c  |  %c  |  %c  \n", board[2][0], board[2][1], board[2][2]);
+    	addstr("     |     |     \n\n");
+    }
 
     refresh();
 }
