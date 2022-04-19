c7f7dbb84629f5c855dbfa47e76892c364708f1e
ANON4620 <swarnadeep.click@gmail.com>
2022-04-19 23:07:38 +0530

    Better computer algorithm

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define PLAYER 'X'
+#define COMPUTER 'O'
+
 void clear_board();
 void draw();
 void player();
@@ -14,11 +17,12 @@ char board[3][3] = {
 	{'4', '5', '6'},
 	{'7', '8', '9'}
 };
-char cur_player = 'X';
-int moves_left = 9, won = 0;
+int moves_left = 9;
+int won = 0;
 
 int main()
 {
+    char cur_player = PLAYER;
     char next_player;
 
     initscr();
@@ -30,15 +34,15 @@ int main()
     
     while(1)
     {
-        if(cur_player == 'X')
+        if(cur_player == PLAYER)
 	{
         	player();
-		next_player = 'O';
+		next_player = COMPUTER;
 	}
 	else
 	{
 		computer();
-		next_player = 'X';
+		next_player = PLAYER;
 	}
 	moves_left--;
 
@@ -105,13 +109,13 @@ void player()
 		break;
     }
 
-    board[row][col] = cur_player;
+    board[row][col] = PLAYER;
 }
 
 void computer()
 {
     char row[moves_left], col[moves_left];
-    int k = 0, random;
+    int k = 0;
 
     // Find all possible moves
     for(int i = 0; i < 3; i++)
@@ -130,7 +134,7 @@ void computer()
     // Check which move can make a win
     for(int i = 0; i < moves_left; i++)
     {
-	board[row[i]][col[i]] = cur_player;
+	board[row[i]][col[i]] = COMPUTER;
 	if(check_win())
 	{
 		won = 1;
@@ -139,21 +143,61 @@ void computer()
 	board[row[i]][col[i]] = ' ';
     }
 
-    // If no move can make a win, check if you can block the player
+    // If no move can make a win, check if computer can block the player
     for(int i = 0; i < moves_left; i++)
     {
-	    board[row[i]][col[i]] = 'X';
+	    board[row[i]][col[i]] = PLAYER;
 	    if(check_win())
 	    {
-		    board[row[i]][col[i]] = cur_player;
+		    board[row[i]][col[i]] = COMPUTER;
 		    return;
 	    }
 	    board[row[i]][col[i]] = ' ';
     }
 
-    // If no move can make a win or a block, pick a random one
-    random = rand() % moves_left;
-    board[row[random]][col[random]] = cur_player;
+    // Check if a move can be made at the center
+    if(board[1][1] == ' ')
+    {
+	    board[1][1] = COMPUTER;
+    }
+    // Check the corners
+    else if(board[0][0] == ' ' && board[0][2] == ' ' && board[2][0] == ' ' && board[2][2] == ' ')
+    {
+	    board[0][0] = COMPUTER;
+    }
+    else if(board[0][0] == PLAYER && board[2][2] == ' ')
+    {
+	    board[2][2] = COMPUTER;
+    }
+    else if(board[2][2] == PLAYER && board[0][0] == ' ')
+    {
+	    board[0][0] = COMPUTER;
+    }
+    else if(board[0][2] == PLAYER && board[2][0] == ' ')
+    {
+	    board[2][0] = COMPUTER;
+    }
+    else if(board[2][0] == PLAYER && board[0][2] == ' ')
+    {
+	    board[0][2] = COMPUTER;
+    }
+    // Check the sides
+    else if(board[0][1] == ' ')
+    {
+	    board[0][1] = COMPUTER;
+    }
+    else if(board[1][0] == ' ')
+    {
+	    board[1][0] = COMPUTER;
+    }
+    else if(board[1][2] == ' ')
+    {
+	    board[1][2] = COMPUTER;
+    }
+    else
+    {
+	    board[2][1] = COMPUTER;
+    }
 }
 
 int check_win()
